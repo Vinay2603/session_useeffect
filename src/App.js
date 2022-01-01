ed620641bee06a25ee28e8daf5c324083c5992a4
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2022-01-01 20:04:12 +0530

    add

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import { Form } from './components/Form';
 import { Todo } from './components/Todo';
 import { Timer } from './components/Timer';
 import { useState } from 'react';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+font-size: 50px;
+text-align: center;
+`;
+
 function App() {
   const [show,setShow ] = useState()
   return (
     <div className="App">
+      <wrapper>
+    agbsaua
+      </wrapper>
       <Todo/>
      {show ?<Timer /> :null }
      <button onClick={()=>{setShow(!show)}}>{show?"HIde":"show"}</button>
